7553eb2555adede454caac7f0cea2a4439f8d0b6
lokka30 <lokka30@protonmail.com>
2020-03-01 17:44:57 +0800

    1.26-RELEASE Build 4

--- src/main/java/io/github/lokka30/levelledmobs/listeners/LCreatureSpawn.java
+++ src/main/java/io/github/lokka30/levelledmobs/listeners/LCreatureSpawn.java
@@ -31,7 +31,6 @@ public class LCreatureSpawn implements Listener {
         if (!e.isCancelled()) {
             final int level; //The mob's level.
             LivingEntity ent = e.getEntity(); //The entity that was just spawned.
-            ent.setCanPickupItems(true);
 
             //Check settings for spawn distance levelling and choose levelling method accordingly.
             if(instance.worldguard && checkRegionFlags(ent)) {
