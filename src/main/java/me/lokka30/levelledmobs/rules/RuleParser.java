0776aa9f5839ac3754c61d43153f9f8f51a29796
lokka30 <lokka30@protonmail.com>
2021-08-23 22:49:33 +0800

    v4.0.0
    Took 8 minutes

---
+++ src/main/java/me/lokka30/levelledmobs/rules/RuleParser.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2020-2021  lokka30. Use of this source code is governed by the GNU AGPL v3.0 license that can be found in the LICENSE.md file.
+ */
+
+package me.lokka30.levelledmobs.rules;
+
+/**
+ * @author lokka30
+ * @since v4.0.0
+ * This class parses rules from the Rules configuration
+ * into Rule objects that are accessed by the plugin.
+ */
+public class RuleParser {
+
+    //TODO
+
+}
