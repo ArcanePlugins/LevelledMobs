6cdd06575c0f437ea6331b665962caad2850192b
lokka30 <lokka30@protonmail.com>
2021-12-23 12:56:38 +0800

    update

---
+++ src/main/java/me/lokka30/levelledmobs/file/external/YamlExternalFile.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2020-2021  lokka30. Use of this source code is governed by the GNU AGPL v3.0 license that can be found in the LICENSE.md file.
+ */
+
+package me.lokka30.levelledmobs.file.external;
+
+import de.leonhard.storage.Yaml;
+import org.jetbrains.annotations.NotNull;
+
+public interface YamlExternalFile extends ExternalFile {
+
+    int getInstalledFileVersion();
+
+    int getLatestFileVersion();
+
+    void migrate();
+
+    @NotNull
+    Yaml getData();
+
+}
---
+++ src/main/java/me/lokka30/levelledmobs/util/math/RangedDouble.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020-2021  lokka30. Use of this source code is governed by the GNU AGPL v3.0 license that can be found in the LICENSE.md file.
+ */
+
+package me.lokka30.levelledmobs.util.math;
+
+import me.lokka30.microlib.maths.Randoms;
+
+public record RangedDouble(double min, double max) {
+
+    public static RangedDouble parse(String val) {
+        String[] split = val.split("-");
+
+        if(split.length == 1) {
+            // not a *ranged* int - only a single int is specified
+            final double d = Double.parseDouble(split[0]);
+            return new RangedDouble(d, d);
+        } else if(split.length == 2) {
+            // ranged int, as two are specified
+            return new RangedDouble(
+                    Double.parseDouble(split[0]),
+                    Double.parseDouble(split[1])
+            );
+        } else {
+            throw new IllegalArgumentException("Incorrect RangedInt formatting for value '" + val + "'.");
+        }
+    }
+
+    public double generateRandom() {
+        return Randoms.generateRandomDouble(min, max);
+    }
+
+}
