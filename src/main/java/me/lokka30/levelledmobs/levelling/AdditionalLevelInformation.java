32fa45c35a1e0c5e290d0101845f4b01a7582dae
lokka30 <lokka30@protonmail.com>
2022-01-07 11:20:24 +0800

    levelling

--- src/main/java/me/lokka30/levelledmobs/levelling/AdditionalLevelInformation.java
+++ src/main/java/me/lokka30/levelledmobs/levelling/AdditionalLevelInformation.java
@@ -1,9 +1,12 @@
 package me.lokka30.levelledmobs.levelling;
 
 /**
+ * When a mob is levelled, a set of these constants
+ * can be provided to describe some circumstances
+ * about the mob being levelled.
  * TODO add description.
  *
- * @author stumper66
+ * @author stumper66, lokka30
  * @since v3.1.0
  * @see me.lokka30.levelledmobs.events.MobPreLevelEvent
  * @see me.lokka30.levelledmobs.events.MobPostLevelEvent
