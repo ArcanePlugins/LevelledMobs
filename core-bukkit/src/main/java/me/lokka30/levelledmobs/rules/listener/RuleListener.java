ed1a1c9601140f676ad1b746122325ab955b8e69
lokka30 <lokka30@protonmail.com>
2022-01-18 15:22:09 +0800

    update

--- core-bukkit/src/main/java/me/lokka30/levelledmobs/rules/listener/RuleListener.java
+++ core-bukkit/src/main/java/me/lokka30/levelledmobs/rules/listener/RuleListener.java
@@ -11,14 +11,14 @@ package me.lokka30.levelledmobs.rules.listener;
 import me.lokka30.levelledmobs.rules.RuleEntry;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.EnumSet;
+import java.util.LinkedList;
 import java.util.Optional;
 
 public record RuleListener(
-        @NotNull String                 listenerId,
-        @NotNull Optional<String>       description,
-        @NotNull HashSet<String>        listenFor,
-        boolean                         ignoreCancelled,
-        @NotNull ArrayList<RuleEntry>   entries
+        @NotNull String                   listenerId,
+        @NotNull Optional<String>         description,
+        @NotNull EnumSet<ListenableEvent> listenFor,
+        boolean                           ignoreCancelled,
+        @NotNull LinkedList<RuleEntry>    entries
 ) {}
