5e950bf7663d0a404bdbf6549b204c6c79b530f7
Penal Buffalo <buffalo246@outlook.com>
2022-05-13 11:45:29 -0500

    v3.5.0 b632
    * custom drops are now processed after the server fully starts. this is only to make lm_items work properly with external plugins

---
+++ src/main/java/me/lokka30/levelledmobs/listeners/ServerStartListener.java
@@ -0,0 +1,23 @@
+package me.lokka30.levelledmobs.listeners;
+
+import me.lokka30.levelledmobs.LevelledMobs;
+import me.lokka30.levelledmobs.misc.FileLoader;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerLoadEvent;
+
+public class ServerStartListener implements Listener {
+    public ServerStartListener(final LevelledMobs main) {
+        this.main = main;
+    }
+
+    private final LevelledMobs main;
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    private void onServerLoadEvent(final ServerLoadEvent event){
+        main.customDropsHandler.customDropsParser.loadDrops(
+                FileLoader.loadFile(main, "customdrops", FileLoader.CUSTOMDROPS_FILE_VERSION)
+        );
+    }
+}
