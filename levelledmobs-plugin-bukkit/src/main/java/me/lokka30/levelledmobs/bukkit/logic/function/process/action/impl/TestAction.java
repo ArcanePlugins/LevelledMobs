5ca324a2fe0d1fca3300415f42934cbedac9892e
lokka30 <lokka30@protonmail.com>
2022-07-04 10:29:28 +0800

    Removed debug message from test action

--- levelledmobs-plugin-bukkit/src/main/java/me/lokka30/levelledmobs/bukkit/logic/function/process/action/impl/TestAction.java
+++ levelledmobs-plugin-bukkit/src/main/java/me/lokka30/levelledmobs/bukkit/logic/function/process/action/impl/TestAction.java
@@ -3,7 +3,6 @@ package me.lokka30.levelledmobs.bukkit.logic.function.process.action.impl;
 import me.lokka30.levelledmobs.bukkit.logic.context.Context;
 import me.lokka30.levelledmobs.bukkit.logic.function.process.Process;
 import me.lokka30.levelledmobs.bukkit.logic.function.process.action.Action;
-import me.lokka30.levelledmobs.bukkit.util.Log;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.configurate.CommentedConfigurationNode;
 
@@ -21,8 +20,5 @@ public class TestAction extends Action {
     /* methods */
 
     @Override
-    public void run(Context context) {
-        //TODO Remove debug
-        Log.inf("DEBUG: Running '__test' action");
-    }
+    public void run(Context context) {}
 }
