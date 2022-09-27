7f98903b462910b140760bd25a49bad3d5b6e789
Penal Buffalo <buffalo246@outlook.com>
2022-09-27 00:18:48 -0500

    v3.7.4 b705
    * added compatibility to minecraft 1.19.0 versions

--- src/main/java/me/lokka30/levelledmobs/nms/NametagSender.java
+++ src/main/java/me/lokka30/levelledmobs/nms/NametagSender.java
@@ -5,9 +5,9 @@ import java.lang.reflect.Method;
 import java.util.Optional;
 
 import me.lokka30.levelledmobs.result.NametagResult;
+import me.lokka30.levelledmobs.util.Utils;
 import me.lokka30.microlib.messaging.MessageUtils;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
@@ -27,16 +27,76 @@ import org.jetbrains.annotations.Nullable;
  */
 public class NametagSender implements NMSUtil {
 
-    public NametagSender(final String nmsVersion, final boolean hasKiori) {
-        this.nmsVersion = nmsVersion;
+    public NametagSender(final @NotNull ServerVersionInfo versionInfo, final boolean hasKiori) {
+        this.versionInfo = versionInfo;
+        this.nmsVersion = versionInfo.getNMSVersion();
         this.hasKiori = hasKiori;
         buildReflection();
     }
 
+    private final ServerVersionInfo versionInfo;
     private final String nmsVersion;
     private final boolean hasKiori;
     private Method resolveStringMethod;
+    private Method emptyComponentMethod;
+    private Method appendComponentMethod;
+    private Method nullToEmptyMethod;
     private Class<?> clazz_CraftChatMessage;
+    private Class<?> clazz_TranslatableComponent;
+
+    private void buildReflection(){
+        String methodName;
+
+        try {
+            // we're only here if we have:
+            // Paper 1.18.0 +
+            // Spigot 1.19.0 +
+
+            Utils.logger.info("vi: " + versionInfo);
+
+            this.clazz_CraftChatMessage = Class.forName(
+                    "org.bukkit.craftbukkit." + nmsVersion + ".util.CraftChatMessage");
+
+            this.resolveStringMethod = clazz_CraftChatMessage.getDeclaredMethod("fromString",
+                    String.class);
+
+            final Class<?> clazz_IChatMutableComponent = Class.forName(
+                    "net.minecraft.network.chat.IChatMutableComponent");
+
+            final Class<?> clazz_IChatBaseComponent = Class.forName(
+                    "net.minecraft.network.chat.IChatBaseComponent");
+
+            if (versionInfo.getMinecraftVersion() < 1.19) {
+                // this is basically TranslatableComponent
+                this.clazz_TranslatableComponent = Class.forName(
+                        "net.minecraft.network.chat.ChatMessage");
+            }
+
+            // 1.19.0 = a, 1.19.1 = b
+            methodName = versionInfo.getRevision() == 0 || versionInfo.getMinecraftVersion() == 1.18
+                    ? "a" : "b";
+
+            // net.minecraft.network.chat.MutableComponent append(net.minecraft.network.chat.Component) ->
+            this.appendComponentMethod = clazz_IChatMutableComponent.getDeclaredMethod(methodName, clazz_IChatBaseComponent);
+
+            if (versionInfo.getMinecraftVersion() >= 1.19) {
+                // 1.19.0 = g, 1.19.1 = h
+                methodName = versionInfo.getRevision() == 0 ? "g" : "h";
+
+                // net.minecraft.network.chat.Component ->
+                //     net.minecraft.network.chat.MutableComponent empty()
+                this.emptyComponentMethod = clazz_IChatBaseComponent.getDeclaredMethod(methodName);
+            }
+            else if (versionInfo.getMinecraftVersion() == 1.18){
+                // 1.18 doesn't have #empty(), instead use #nullToEmpty()
+                // net.minecraft.network.chat.Component -> qk:
+                //    net.minecraft.network.chat.Component nullToEmpty(java.lang.String) -> a
+                this.nullToEmptyMethod = clazz_IChatBaseComponent.getDeclaredMethod("a", String.class);
+            }
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void sendNametag(final @NotNull LivingEntity livingEntity, @NotNull NametagResult nametag,
                             @NotNull Player player, final boolean doAlwaysVisible) {
@@ -116,42 +176,61 @@ public class NametagSender implements NMSUtil {
                 null;
 
         final Component mobNameComponent = nametag.overriddenName == null ?
-                Component.translatable(getTranslationKey(livingEntity.getType())) :
-                Component.literal(nametag.overriddenName);
+                getTranslatableComponent(getTranslationKey(livingEntity.getType())) :
+                Component.nullToEmpty(nametag.overriddenName);
 
-        MutableComponent comp = Component.empty();
+        Object comp = null;
         try {
+            // MutableComponent comp = Component.empty();
+            comp = getEmptyComponent();
+
             if (leftText != null) {
-                comp = comp.append(((Component[]) this.resolveStringMethod.invoke(this.clazz_CraftChatMessage, leftText))[0]);
+                // comp.append(Component);
+                appendComponentMethod.invoke(comp, ((Component[]) this.resolveStringMethod.invoke(this.clazz_CraftChatMessage, leftText))[0]);
             }
-            comp.append(mobNameComponent);
+
+            appendComponentMethod.invoke(comp, mobNameComponent);
             if (rightText != null) {
-                comp = comp.append(((Component[]) this.resolveStringMethod.invoke(this.clazz_CraftChatMessage, rightText))[0]);
+                // comp.append(Component);
+                appendComponentMethod.invoke(comp, ((Component[]) this.resolveStringMethod.invoke(this.clazz_CraftChatMessage, rightText))[0]);
             }
 
         } catch (InvocationTargetException | IllegalAccessException e) {
             e.printStackTrace();
         }
 
-        return Optional.of(comp);
+        return comp == null ?
+                Optional.empty() : Optional.of((Component) comp);
+    }
+
+    private @Nullable Object getEmptyComponent() throws InvocationTargetException, IllegalAccessException {
+        if (versionInfo.getMinecraftVersion() >= 1.19) {
+            return this.emptyComponentMethod.invoke(null);
+        }
+        else {
+            return this.nullToEmptyMethod.invoke(null, (Object) null);
+        }
+    }
+
+    private @Nullable Component getTranslatableComponent(final @NotNull String key){
+        if (versionInfo.getMinecraftVersion() >= 1.19){
+            return Component.translatable(key);
+        }
+        else {
+            Component result = null;
+            try {
+                result = (Component) this.clazz_TranslatableComponent.getConstructor(String.class).newInstance(key);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return result;
+        }
     }
 
     @SuppressWarnings("deprecation")
     private String getTranslationKey(final org.bukkit.entity.@NotNull EntityType type) {
         return net.minecraft.world.entity.EntityType.byString(type.getName()).map(net.minecraft.world.entity.EntityType::getDescriptionId).orElse(null);
-    }
-
-    private void buildReflection(){
-        try {
-            this.clazz_CraftChatMessage = Class.forName(
-                    "org.bukkit.craftbukkit." + nmsVersion + ".util.CraftChatMessage");
-
-            this.resolveStringMethod = clazz_CraftChatMessage.getDeclaredMethod("fromString",
-                    String.class);
-
-        } catch (ClassNotFoundException | NoSuchMethodException e) {
-            e.printStackTrace();
-        }
     }
 
     private @Nullable String resolveText(final @Nullable String text){
