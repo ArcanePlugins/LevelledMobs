f6e8ff8ae23a634eee3cf132522a70e6d5321167
Penal Buffalo <buffalo246@outlook.com>
2023-05-05 10:54:22 -0500

    v3.10.1 b753
    fix accidental revert

--- src/main/java/me/lokka30/levelledmobs/rules/RulesManager.java
+++ src/main/java/me/lokka30/levelledmobs/rules/RulesManager.java
@@ -402,7 +402,7 @@ public class RulesManager {
 
         for (final RuleInfo ruleInfo : lmEntity.getApplicableRules()) {
             if (ruleInfo.playerLevellingOptions != null) {
-                if (levellingOptions == null)
+                if (levellingOptions == null || !levellingOptions.doMerge)
                     levellingOptions = ruleInfo.playerLevellingOptions.cloneItem();
                 else
                     levellingOptions.mergeRule(ruleInfo.playerLevellingOptions);
