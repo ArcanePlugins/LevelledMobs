09e0c5522dd081a2da35b4a8555274bb1a49394a
Penal Buffalo <buffalo246@outlook.com>
2025-06-23 19:42:39 -0500

    v4.3.2 b124
    * full Minecraft 1.21.6 support
    * bump Kotlin version to 2.2.0

--- levelledmobs-plugin/build.gradle.kts
+++ levelledmobs-plugin/build.gradle.kts
@@ -7,7 +7,7 @@ version = version
 
 plugins {
     id("java")
-    kotlin("jvm") version "2.1.21"
+    kotlin("jvm") version "2.2.0"
     id("com.gradleup.shadow") version "9.0.0-beta16"
     id("idea")
     id("maven-publish")
@@ -28,7 +28,7 @@ idea {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.21")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.2.0")
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT") {
         exclude(group = "com.google.guava", module = "guava")
         exclude(group = "com.google.code.gson", module = "gson")
